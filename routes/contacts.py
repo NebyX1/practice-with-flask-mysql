be937ae4c9efd531d93b568f7fc8a7db6f16740b
NebyX1 <blackcat_neme@hotmail.com>
2023-04-01 04:31:20 -0300

    Primer Commit

---
+++ routes/contacts.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from models.contact import Contact
+from utils.db import db
+
+contacts = Blueprint('contacts', __name__)
+
+
+@contacts.route('/')
+def home():
+    contacts = Contact.query.all()
+    return render_template('index.html', contacts = contacts)
+
+@contacts.route('/about')
+def about():
+    return render_template('about.html')
+
+@contacts.route('/delete/<id>', methods=['POST', 'GET'])
+def delete(id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    flash("Contact deleted successfully!")
+    return redirect(url_for('contacts.home'))
+
+@contacts.route('/update/<id>', methods=['POST', 'GET'])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == 'POST':
+        contact.fullname = request.form["fullname"]
+        contact.email = request.form["email"]
+        contact.phone = request.form["phone"]
+        db.session.commit()
+        flash("Contact updated successfully!")
+        return redirect(url_for('contacts.home'))
+    else:
+        return render_template('update.html', contact = contact)
+
+@contacts.route('/new', methods=['POST'])
+def add_contact():
+    print(request.form)
+    fullname = request.form["fullname"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    new_contact = Contact(fullname, email, phone)
+    db.session.add(new_contact)
+    db.session.commit()
+    flash("Contact added successfully!")
+    return redirect(url_for('contacts.home'))
